2d600673f4105db3351ce588eb539470cf6e3e99
jothamardel <ardelmbiplang@gmail.com>
2022-04-28 22:33:48 +0100

    fix: fixing api url

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,9 +4,9 @@ import axios from "axios";
 let API_URL = process.env.REACT_APP_API;
 console.log(API_URL)
 export function httpLoginUser(data) {
-	return axios.post(`${API_URL}/login`, data);
+	return axios.post(`${API_URL}login`, data);
 }
 
 export function httpRegisterUser(data) {
-	return axios.post(`${API_URL}/auth/register`, data);
+	return axios.post(`${API_URL}auth/register`, data);
 }
